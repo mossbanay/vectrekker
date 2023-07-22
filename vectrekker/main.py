0545c2c5006f66f7ca5b48fc2355cba241e94bb8
moss <moss@banay.me>
2023-07-22 21:41:32 +1000

    refactoring

--- vectrekker/main.py
+++ vectrekker/main.py
@@ -2,7 +2,8 @@ import os
 import re
 import sqlite3
 from pathlib import Path
-from typing import Iterable
+from types import TracebackType
+from typing import Iterable, Optional, Type
 
 import openai
 import pinecone
@@ -16,12 +17,14 @@ from pydantic_settings import BaseSettings
 class BaseConfig(BaseSettings):
     content_folder: str
     content_regex: str = r".*\.md$"
+    dimension_size: int = 1536
+    metric: str = "cosine"
 
 
 class PineconeConfig(BaseSettings):
     api_key: str = Field(..., env="PINECONE_API_KEY")
     environment: str = Field(..., env="PINECONE_ENVIRONMENT")
-    index_name: str = Field(..., env="PINECONE_INDEX")
+    index_name: str = Field(..., default="vectrekker", env="PINECONE_INDEX")
 
 
 class Config(BaseSettings):
@@ -50,11 +53,24 @@ def stat_modified_time(path: Path):
 
 
 class FileCache:
-    """A class for interacting with the SQLite cache."""
+    """A simple file cache for storing the last edit time of a file."""
 
     def __init__(self, path: Path):
         self.path = path
+        self.conn = None
+
+    def __enter__(self):
         self.conn = self.create_db(self.path)
+        return self
+
+    def __exit__(
+        self,
+        _exc_type: Optional[Type[BaseException]],
+        _exc: Optional[BaseException],
+        _traceback: Optional[TracebackType],
+    ):
+        self.conn.close()
+        self.conn = None
 
     @staticmethod
     def create_db(path: Path):
@@ -63,27 +79,19 @@ class FileCache:
         if not path.exists():
             conn = sqlite3.connect(path)
             conn.execute(
-                """
-                CREATE TABLE IF NOT EXISTS files (
-                    path TEXT PRIMARY KEY,
-                    last_edit_time INTEGER
-                )
-                """
+                "CREATE TABLE IF NOT EXISTS files (path TEXT PRIMARY KEY, last_edit_time INTEGER)"
             )
             conn.commit()
             conn.close()
 
-        conn = sqlite3.connect(path)
-        return conn
+        return sqlite3.connect(path)
 
     def get_edit_time(self, path: Path):
-        """Get the last edit time of a file from the database."""
-        query = "SELECT last_edit_time FROM files WHERE path = ?"
+        """Get the last edit time of a file from the database or zero if not seen before."""
         cursor = self.conn.cursor()
-        cursor.execute(query, (str(path),))
-
+        cursor.execute("SELECT last_edit_time FROM files WHERE path = ?", (str(path),))
         result = cursor.fetchone()
-        return result[0] if result else 0
+        return result[0] or 0
 
     def reset_edit_time(self, path: Path):
         """Reset the last edit time and hash of a file in the database."""
@@ -97,10 +105,6 @@ class FileCache:
         cursor.execute(query, (str(path), last_edit_time))
         self.conn.commit()
 
-    def close(self):
-        """Close the connection to the database."""
-        self.conn.close()
-
 
 def walk(path: Path) -> Iterable[Path]:
     for p in Path(path).iterdir():
@@ -113,29 +117,24 @@ def walk(path: Path) -> Iterable[Path]:
 def main(dry_run: bool = typer.Option(False)):
     """VecTrekker
 
-    VecTrekker is a simple utility used to index content on disk and upsert it
-    to a vector store for usage with LLMs.
+    VecTrekker is a simple utility used to index text content on disk, embed it
+    into vectors and upsert it to a vector store for usage with LLMs.
     """
 
     config = load_config()
-
-    candidate_files: list[Path] = []
+    content_regex = re.compile(config.base.content_regex)
     files_to_reindex: list[Path] = []
 
-    content_regex = re.compile(config.base.content_regex)
+    with FileCache(Path.home() / ".vectrekker" / "cache.db") as conn:
+        for f in walk(Path(config.base.content_folder)):
+            if not content_regex.match(str(f)):
+                continue
 
-    for f in walk(Path(config.base.content_folder)):
-        if content_regex.match(str(f)):
-            candidate_files.append(f)
+            last_edit_time = conn.get_edit_time(entry)
+            current_edit_time = stat_modified_time(entry)
 
-    conn = FileCache(Path.home() / ".vectrekker" / "cache.db")
-
-    for entry in candidate_files:
-        last_edit_time = conn.get_edit_time(entry)
-        current_edit_time = stat_modified_time(entry)
-
-        if current_edit_time > last_edit_time:
-            files_to_reindex.append(entry)
+            if current_edit_time > last_edit_time:
+                files_to_reindex.append(entry)
 
     print(f"{len(files_to_reindex)} file(s) have changed")
 
@@ -146,20 +145,21 @@ def main(dry_run: bool = typer.Option(False)):
         api_key=config.pinecone.api_key, environment=config.pinecone.environment
     )
 
-    if "vectrekker" not in pinecone.list_indexes():
+    if config.pinecone.index_name not in pinecone.list_indexes():
         pinecone.create_index(
-            name=config.pinecone.index_name, dimension=1536, metric="cosine"
+            name=config.pinecone.index_name,
+            dimension=config.base.dimension_size,
+            metric=config.base.metric,
         )
 
     index = pinecone.Index(index_name=config.pinecone.index_name)
-
     encoder = tiktoken.get_encoding("cl100k_base")
 
     for entry in files_to_reindex:
         file_contents = entry.read_text()
         toks = encoder.encode(file_contents)
 
-        # Currently we assume that all documents are less than 8191 tokens
+        # TODO: Currently we assume that all documents are less than 8191 tokens
         assert len(toks) < 8191, "Document is too long, splitting not yet supported"
 
         embd = openai.Embedding.create(
@@ -167,7 +167,9 @@ def main(dry_run: bool = typer.Option(False)):
         )["data"][0]["embedding"]
 
         index.upsert(vectors=[(str(entry), embd, {})])
-        conn.reset_edit_time(entry)
+
+        with FileCache(Path.home() / ".vectrekker" / "cache.db") as conn:
+            conn.reset_edit_time(entry)
 
 
 if __name__ == "__main__":
