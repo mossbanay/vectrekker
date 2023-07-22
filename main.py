d8bf9926687831134769348de15e0a84e0af2107
moss <moss@banay.me>
2023-07-22 18:51:28 +1000

    Cleanup

--- main.py
+++ main.py
@@ -4,36 +4,43 @@ import sqlite3
 import time
 import toml
 from pathlib import Path
+from typing import Any
 
 CONTENT_REGEX_DEFAULT = r".*\.md$"
 
-def default_content_regex():
-    return CONTENT_REGEX_DEFAULT
-
-def stat_modified_time(path):
+def stat_modified_time(path: Path):
+    """Returns the last modified time of the given path."""
     modified_time = os.path.getmtime(path)
     return int(modified_time)
 
-class Config:
-    def __init__(self, content_folder, content_regex=default_content_regex()):
-        self.content_folder = content_folder
-        self.content_regex = content_regex
-
 class FileCache:
-    def __init__(self, path):
-        self.conn = sqlite3.connect(path)
-        if not os.path.exists(path):
-            self.conn.execute(
+    """A class for interacting with the SQLite cache."""
+    def __init__(self, path: Path):
+        self.path = path
+        self.conn = self.create_db(self.path)
+
+    @staticmethod
+    def create_db(path: Path):
+        """Create a new database or connect to an existing one."""
+
+        if not path.exists():
+            conn = sqlite3.connect(path)
+            conn.execute(
                 """
-                CREATE TABLE files (
+                CREATE TABLE IF NOT EXISTS files (
                     path TEXT PRIMARY KEY,
-                    last_edit_time INTEGER,
-                    hash TEXT
+                    last_edit_time INTEGER
                 )
                 """
             )
+            conn.commit()
+            conn.close()
 
-    def get_edit_time(self, path):
+        conn = sqlite3.connect(path)
+        return conn
+
+    def get_edit_time(self, path: Path):
+        """Get the last edit time of a file from the database."""
         query = "SELECT last_edit_time FROM files WHERE path = ?"
         cursor = self.conn.cursor()
         cursor.execute(query, (path,))
@@ -41,45 +48,48 @@ class FileCache:
         result = cursor.fetchone()
         return result[0] if result else 0
 
-    def reset_edit_time(self, path):
+    def reset_edit_time(self, path: Path):
+        """Reset the last edit time and hash of a file in the database."""
         query = """
-        INSERT OR REPLACE INTO files (path, last_edit_time, hash)
-        VALUES (?, ?, ?)
+        INSERT OR REPLACE INTO files (path, last_edit_time)
+        VALUES (?, ?)
         """
-        cursor = self.conn.cursor()
         last_edit_time = stat_modified_time(path)
-        hash = "TODO"
-        cursor.execute(query, (path, last_edit_time, hash))
 
+        cursor = self.conn.cursor()
+        cursor.execute(query, (path, last_edit_time))
         self.conn.commit()
 
-def load_config():
-    config_folder_path = "/Users/moss/.vectrekker"
-    os.makedirs(config_folder_path, exist_ok=True)
+    def close(self):
+        """Close the connection to the database."""
+        self.conn.close()
 
-    config_file = os.path.join(config_folder_path, "config.toml")
-    if not os.path.exists(config_file):
-        with open(config_file, 'w') as f:
-            pass
+def load_config() -> dict[str, Any]:
+    """Load the configuration from a TOML file."""
+    config_folder_path = Path.home() / ".vectrekker"
+    config_folder_path.mkdir(exist_ok=True)
+
+    config_file = config_folder_path / "config.toml"
+    config_file.touch(exist_ok=True)
 
     with open(config_file, 'r') as f:
         config_file_contents = f.read()
 
     config = toml.loads(config_file_contents)
-    return Config(config['content_folder'], config.get('content_regex', default_content_regex()))
+    return config
 
 def main():
     config = load_config()
-    content_regex = re.compile(config.content_regex)
+    content_regex = re.compile(config.get('content_regex', CONTENT_REGEX_DEFAULT))
 
     candidate_files = []
 
-    for dirpath, dirnames, filenames in os.walk(config.content_folder):
-        for filename in filenames:
-            if content_regex.match(filename):
-                candidate_files.append(os.path.join(dirpath, filename))
+    for root, _, files in os.walk(config['content_folder']):
+        for f in files:
+            if content_regex.match(f):
+                candidate_files.append(os.path.join(root, f))
 
-    conn = FileCache("/Users/moss/.vectrekker/cache.db")
+    conn = FileCache(Path.home() / ".vectrekker" / "cache.db")
     for entry in candidate_files:
         print(entry)
         last_edit_time = conn.get_edit_time(entry)
@@ -88,7 +98,7 @@ def main():
         print(last_edit_time, current_edit_time)
 
         if current_edit_time > last_edit_time:
-            print("File has changed:", entry)
+            print(f"File has changed: {entry}")
 
             # TODO: Tokenize the file and submit embedding to vector store
 
